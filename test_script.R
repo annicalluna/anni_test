a3f997a0fb677b6ff6f0423e7b73a98fbb547a71
unknown <annipyy@mail.muni.cz>
2023-05-18 15:32:36 +0200

    xx

--- test_script.R
+++ test_script.R
@@ -1,3 +1,13 @@
 get_yesterday <- function() {
   return(Sys.Date() - 1)
 }
+library(usethis)
+edit_r_environ()
+GITHUB_PAT = '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'
+
+use_git(protocol ='https', auth_token = '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')
+install.packages('gitcreds')
+library(gitcreds)
+
+gitcreds_set()
+use_github()
